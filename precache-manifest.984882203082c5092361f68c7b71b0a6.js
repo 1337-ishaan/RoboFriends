0b643989a30c1eaedc345f2fd5971eb5a5eaee2e
1337-ishaan <1337.ishaan@gmail.com>
2020-05-05 00:59:03 +0530

    Updates

---
+++ precache-manifest.984882203082c5092361f68c7b71b0a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84e126aa53362b6b534b0eeedc4fdd5",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "17d4117ae9cc980df062",
+    "url": "/Robofriends/static/css/2.708eaab9.chunk.css"
+  },
+  {
+    "revision": "379f2a27d7402b621c15",
+    "url": "/Robofriends/static/css/main.867e9362.chunk.css"
+  },
+  {
+    "revision": "17d4117ae9cc980df062",
+    "url": "/Robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379f2a27d7402b621c15",
+    "url": "/Robofriends/static/js/main.2777a37c.chunk.js"
+  },
+  {
+    "revision": "351c2394620c1f8f70b1",
+    "url": "/Robofriends/static/js/runtime-main.32cacffe.js"
+  }
+]);
